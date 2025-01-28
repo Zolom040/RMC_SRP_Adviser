cc90e17cf49d84cabaf2c3c6d8ead583e030126c
Zolom040 <nicro040@gmail.com>
2025-01-28 14:31:40 +0300

    Inshurance add
    Inshurance and killmail links added. Ali ticker added

--- eve_api_requests.py
+++ eve_api_requests.py
@@ -8,7 +8,7 @@ class EveRequester:
         print("done")
 
     # Запрос на получение всех потерь заданного альянса в заданный период (1 месяц)
-    # Ответ -  killmail_id, zkb [location, hash, fittValue (isk), dropValue (isk), destroyedValue, totalvalue, points, npc, solo, awox, labels ]
+    # Ответ -  killmail_id, zkb [location, hash, fittedValue (isk), dropValue (isk), destroyedValue, totalvalue, points, npc, solo, awox, labels ]
     def get_zkillboard_killmails_for_alliance(self, alliance_id=None, year=2025, month=1, page=1):
         time.sleep(2)
         url = f"https://zkillboard.com/api/losses/allianceID/{alliance_id}/year/{year}/month/{month}/page/{page}/"
@@ -20,6 +20,70 @@ class EveRequester:
     # items, position [x,y,z], ship_type_id, faction_id]
     def get_killmail_details(self, killmail_id, killmail_hash):
         url = f"https://esi.evetech.net/latest/killmails/{killmail_id}/{killmail_hash}/"
+        retries_left = MAX_RETRIES
+
+        while retries_left > 0:
+            try:
+                response = requests.get(url, timeout=5)
+
+                if response.status_code == 200:
+                    response_json = response.json()
+
+                    if "error" not in response_json:
+                        return response_json
+                    else:
+                        raise ValueError("Response contains an error")
+
+                else:
+                    print(f"Ошибка: {response.status_code}")
+                    retries_left -= 1
+                    time.sleep(60)  # Ждем перед новой попыткой
+                    continue
+
+            except Exception as e:
+                print(f"Ошибка при получении данных: {e}")
+                retries_left -= 1
+                time.sleep(60)  # Ждем перед новой попыткой
+                continue
+
+        # Если мы вышли из цикла, значит исчерпаны все попытки
+        print("Все попытки исчерпаны, не удалось получить данные.")
+        return None
+
+    def get_Inshurances(self):
+        url = f"https://esi.evetech.net/dev/insurance/prices/"
+        retries_left = MAX_RETRIES
+
+        while retries_left > 0:
+            try:
+                response = requests.get(url, timeout=5)
+
+                if response.status_code == 200:
+                    response_json = response.json()
+
+                    if "error" not in response_json:
+                        return response_json
+                    else:
+                        raise ValueError("Response contains an error")
+
+                else:
+                    print(f"Ошибка: {response.status_code}")
+                    retries_left -= 1
+                    time.sleep(60)  # Ждем перед новой попыткой
+                    continue
+
+            except Exception as e:
+                print(f"Ошибка при получении данных: {e}")
+                retries_left -= 1
+                time.sleep(60)  # Ждем перед новой попыткой
+                continue
+
+        # Если мы вышли из цикла, значит исчерпаны все попытки
+        print("Все попытки исчерпаны, не удалось получить данные.")
+        return None
+
+    def get_Aliance_Info(self, alliance_id):
+        url = f"https://esi.evetech.net/dev/alliances/{alliance_id}/"
         retries_left = MAX_RETRIES
 
         while retries_left > 0:
--- main.py
+++ main.py
@@ -22,54 +22,79 @@ def main():
     year = last_month.year
     print(f"month {month} year {year}")
     requester = EveRequester()
+    inshu = requester.get_Inshurances()
+    inshurances = dict()
+    for ins in inshu:
+        for level in ins["levels"]:
+            if level["name"] == "Platinum":
+                inshurances[ins["type_id"]] = level["payout"]
+
     Members = dict()
     for aliance in loader.alliances:
+        AliInfo = requester.get_Aliance_Info(aliance)
+        AlianceTicker = AliInfo["ticker"]
         for p in range(1,15):
             aliance_killmails = requester.get_zkillboard_killmails_for_alliance(aliance,year,month,p)
             for km in aliance_killmails:
                 killmail_id = km["killmail_id"]
                 killmail_hash = km["zkb"]["hash"]
-                totalValue = km["zkb"]["totalValue"]
-                kmd = requester.get_killmail_details(killmail_id, killmail_hash)
+                totalValue = km["zkb"]["fittedValue"]
+                if not km["zkb"]["npc"]:
+                    kmd = requester.get_killmail_details(killmail_id, killmail_hash)
 
-                if "victim" in kmd:
-                    if "character_id" in kmd["victim"]:
-                        char_id = kmd["victim"]["character_id"]
+                    if "victim" in kmd:
+                        if "character_id" in kmd["victim"]:
+                            char_id = kmd["victim"]["character_id"]
+                        else:
+                            print(f"какая то ошибка с json в киле {killmail_id} пропущу это килмыло. ктото недополучит компенс")
+                            continue
                     else:
                         print(f"какая то ошибка с json в киле {killmail_id} пропущу это килмыло. ктото недополучит компенс")
                         continue
-                else:
-                    print(f"какая то ошибка с json в киле {killmail_id} пропущу это килмыло. ктото недополучит компенс")
-                    continue
 
-                if char_id not in Members:
-                    new_member = {
-                        "ID": char_id,
-                        "Name": requester.get_CharacterInfo(kmd["victim"]["character_id"])["name"],
-                        "total_cost": 0,
-                        "natur_compens_ids" : []
-                    }
-                    Members[new_member["ID"]] = new_member
+                    if char_id not in Members:
+                        new_member = {
+                            "ID": char_id,
+                            "Name": requester.get_CharacterInfo(kmd["victim"]["character_id"])["name"],
+                            "Aliance": AlianceTicker,
+                            "total_cost": 0,
+                            "natur_compens_ids" : [],
+                            "dps_links": [],
+                            "support_links": [],
+                            "tackle_links": [],
+                            "valuble_links": [],
+                            "capital_links": []
+                        }
+                        Members[new_member["ID"]] = new_member
 
-                sh_id = kmd["victim"]["ship_type_id"]
-                ship_id = str(sh_id)
-                if ship_id in loader.support_ships:
-                    Members[char_id]["total_cost"] += totalValue
-                    print(Members[char_id]["Name"] + " Большой молодец ")
-                elif ship_id in loader.tackle_ships:
-                    Members[char_id]["total_cost"] += totalValue
-                    print(Members[char_id]["Name"] + " молодец ")
-                elif ship_id in loader.dps_ships:
-                    Members[char_id]["total_cost"] += totalValue * 0.7
-                    print(Members[char_id]["Name"] + " норм ")
-                elif ship_id in loader.vaiuble_ships:
-                    Members[char_id]["natur_compens_ids"].append(ship_id)
-                    print(Members[char_id]["Name"] + " молодец ")
-                elif ship_id in loader.capital_ships:
-                    Members[char_id]["natur_compens_ids"].append(ship_id)
-                    print(Members[char_id]["Name"] + " Вообще молодец ")
+                    sh_id = kmd["victim"]["ship_type_id"]
+                    ship_id = str(sh_id)
+                    if ship_id in loader.support_ships:
+                        Members[char_id]["total_cost"] += totalValue - inshurances[sh_id]
+                        Members[char_id]["support_links"].append(f"https://zkillboard.com/kill/{killmail_id}/")
+                        print(Members[char_id]["Name"] + " Большой молодец ")
+                    elif ship_id in loader.tackle_ships:
+                        Members[char_id]["total_cost"] += totalValue - inshurances[sh_id]
+                        Members[char_id]["tackle_links"].append(f"https://zkillboard.com/kill/{killmail_id}/")
+                        print(Members[char_id]["Name"] + " молодец ")
+                    elif ship_id in loader.dps_ships:
+                        Members[char_id]["total_cost"] += (totalValue - inshurances[sh_id]) * 0.7
+                        Members[char_id]["dps_links"].append(f"https://zkillboard.com/kill/{killmail_id}/")
+                        print(Members[char_id]["Name"] + " норм ")
+                    elif ship_id in loader.vaiuble_ships:
+                        Members[char_id]["natur_compens_ids"].append(ship_id)
+                        Members[char_id]["total_cost"] -= inshurances[sh_id]
+                        Members[char_id]["valuble_links"].append(f"https://zkillboard.com/kill/{killmail_id}/")
+                        print(Members[char_id]["Name"] + " молодец ")
+                    elif ship_id in loader.capital_ships:
+                        Members[char_id]["natur_compens_ids"].append(ship_id)
+                        Members[char_id]["total_cost"] -= inshurances[sh_id]
+                        Members[char_id]["capital_links"].append(f"https://zkillboard.com/kill/{killmail_id}/")
+                        print(Members[char_id]["Name"] + " Вообще молодец ")
+                    else:
+                        print(Members[char_id]["Name"] + " крабина ")
                 else:
-                    print(Members[char_id]["Name"] + " крабина ")
+                    print("Килл от нпц")
             time.sleep(2)
 
     # Фильтрация словаря
